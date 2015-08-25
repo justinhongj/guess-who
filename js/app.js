a056c4d5ddc345892d1e7474e32ff5921a38af42
Justin Hong <justinhong.j@gmail.com>
2015-08-25 13:54:20 -0700

    modify properties of characters

--- js/app.js
+++ js/app.js
@@ -12,31 +12,31 @@ function Character(name, sex, hair, glasses, facialHair, shirt, outerwear, locat
 	this.location = location;
 }
 
-var adam = new Character('Adam West', 'male', 'grey', false, false, 'collared', true, 'images/Adam-West.jpg');
-var beth = new Character('Beth', 'female', 'blonde', false, false, 'collared', false, 'images/Beth.JPG');
-var cyril = new Character('Cyril', 'male', 'black', true, false, 'collared', true, 'images/Cyril.jpg');
-var grandpa = new Character('Grandpa', 'male', 'grey', false, false, null, true, 'images/Grandpa.jpg');
-var krieger = new Character('Dr. Krieger', 'male', 'brown', false, true, 'collared', true, 'images/Krieger.jpg');
-var linda = new Character('Linda', 'female', 'black', true, false, null, false, 'images/Linda.jpg');
-var lois = new Character('Lois', 'female', 'red', false, false, 'collared', false, 'images/Lois.jpg');
-var meg = new Character('Meg', 'female', 'brown', true, false, null, false, 'images/Meg.jpg');
-var mort = new Character('Mort', 'male', 'red', true, false, 'collared', true, 'images/Mort.gif');
-var morty = new	Character('Morty', 'male', 'brown', false, false, null, false, 'images/Morty.JPG');
-var pam = new Character('Pam', 'female', 'blonde', false, false, 'collared', false, 'images/Pam.jpg');
-var peter = new Character('Peter', 'male', 'brown', true, false, 'collared', false, 'images/Peter.jpg');
-var ray = new Character('Ray', 'male', 'blonde', false, true, 'collared', true, 'images/Ray.jpg');
-var ron = new Character('Ron', 'male', 'red', false, true, 'collared', false, 'images/Ron.jpg');
-var summer = new Character('Summer', 'female', 'red', false, false, null, false, 'images/Summer.JPG');
-var archer = new Character('Archer', 'male', 'black', false, false, 'collared', true, 'images/Archer.png');
-var bob = new Character('Bob', 'male', 'black', false, true, null, true, 'images/Bob.png');
-var fischoeder = new Character('Mr. Fischoeder', 'male', 'grey', false, false, 'collared', true, 'images/Fischoeder.png');
-var goldenfold = new Character('Mr. Goldenfold', 'male', 'black', false, true, 'collared', true, 'images/Goldenfold.png');
-var hugo = new Character('Hugo', 'male', 'blonde', false, false, 'collared', false, 'images/Hugo.png');
-var jessica = new Character('Jessica', 'female', 'red', false, false, 'collared', false, 'images/Jessica.png');
-var malory = new Character('Malory', 'female', 'grey', false, false, null, true, 'images/Malory.png');
-var rick = new Character('Rick', 'male', 'grey', false, false, null, true, 'images/Rick.png');
-var steve = new Character('Uncle Steve', 'male', 'brown', false, true, 'collared', false, 'images/Steve.png');
-var tina = new Character('Tina', 'female', 'black', true, false, null, false, 'images/Tina.png');
+var adam = new Character('Adam West', 'Male', 'Grey Hair', 'No Glasses', 'No Facial Hair', 'Collared Shirt', 'Outerwear', 'images/Adam-West.jpg');
+var beth = new Character('Beth', 'Female', 'Blonde', 'No Glasses', 'No Facial Hair', 'Collared Shirt', 'No Outerwear', 'images/Beth.JPG');
+var cyril = new Character('Cyril', 'male', 'Black Hair', 'Glasses', 'No Facial Hair', 'Collared Shirt', 'Outerwear', 'images/Cyril.jpg');
+var grandpa = new Character('Grandpa', 'male', 'Grey Hair', 'No Glasses', 'No Facial Hair', 'Non-Collared Shirt', 'Outerwear', 'images/Grandpa.jpg');
+var krieger = new Character('Dr. Krieger', 'male', 'Brown Hair', 'No Glasses', 'Facial Hair', 'Collared Shirt', 'Outerwear', 'images/Krieger.jpg');
+var linda = new Character('Linda', 'female', 'Black Hair', 'Glasses', 'No Facial Hair', 'Non-Collared Shirt', 'No Outerwear', 'images/Linda.jpg');
+var lois = new Character('Lois', 'female', 'Red Hair', 'No Glasses', 'No Facial Hair', 'Collared Shirt', 'No Outerwear', 'images/Lois.jpg');
+var meg = new Character('Meg', 'female', 'Brown Hair', 'Glasses', 'No Facial Hair', 'Non-Collared Shirt', 'No Outerwear', 'images/Meg.jpg');
+var mort = new Character('Mort', 'male', 'Red Hair', 'Glasses', 'No Facial Hair', 'Collared Shirt', 'Outerwear', 'images/Mort.gif');
+var morty = new	Character('Morty', 'male', 'Brown Hair', 'No Glasses', 'No Facial Hair', 'Non-Collared Shirt', 'No Outerwear', 'images/Morty.JPG');
+var pam = new Character('Pam', 'female', 'Blonde', 'No Glasses', 'No Facial Hair', 'Collared Shirt', 'No Outerwear', 'images/Pam.jpg');
+var peter = new Character('Peter', 'male', 'Brown Hair', 'Glasses', 'No Facial Hair', 'Collared Shirt', 'No Outerwear', 'images/Peter.jpg');
+var ray = new Character('Ray', 'male', 'Blonde', 'No Glasses', 'Facial Hair', 'Collared Shirt', 'Outerwear', 'images/Ray.jpg');
+var ron = new Character('Ron', 'male', 'Red Hair', 'No Glasses', 'Facial Hair', 'Collared Shirt', 'No Outerwear', 'images/Ron.jpg');
+var summer = new Character('Summer', 'female', 'Red Hair', 'No Glasses', 'No Facial Hair', 'Non-Collared Shirt', 'No Outerwear', 'images/Summer.JPG');
+var archer = new Character('Archer', 'male', 'Black Hair', 'No Glasses', 'No Facial Hair', 'Collared Shirt', 'Outerwear', 'images/Archer.png');
+var bob = new Character('Bob', 'male', 'Black Hair', 'No Glasses', 'Facial Hair', 'Non-Collared Shirt', 'Outerwear', 'images/Bob.png');
+var fischoeder = new Character('Mr. Fischoeder', 'male', 'Grey Hair', 'No Glasses', 'No Facial Hair', 'Collared Shirt', 'Outerwear', 'images/Fischoeder.png');
+var goldenfold = new Character('Mr. Goldenfold', 'male', 'Black Hair', 'No Glasses', 'Facial Hair', 'Collared Shirt', 'Outerwear', 'images/Goldenfold.png');
+var hugo = new Character('Hugo', 'male', 'Blonde', 'No Glasses', 'No Facial Hair', 'Collared Shirt', 'No Outerwear', 'images/Hugo.png');
+var jessica = new Character('Jessica', 'female', 'Red Hair', 'No Glasses', 'No Facial Hair', 'Collared Shirt', 'No Outerwear', 'images/Jessica.png');
+var malory = new Character('Malory', 'female', 'Grey Hair', 'No Glasses', 'No Facial Hair', 'Non-Collared Shirt', 'Outerwear', 'images/Malory.png');
+var rick = new Character('Rick', 'male', 'Grey Hair', 'No Glasses', 'No Facial Hair', 'Non-Collared Shirt', 'Outerwear', 'images/Rick.png');
+var steve = new Character('Uncle Steve', 'male', 'Brown Hair', 'No Glasses', 'Facial Hair', 'Collared Shirt', 'No Outerwear', 'images/Steve.png');
+var tina = new Character('Tina', 'female', 'Black Hair', 'Glasses', 'No Facial Hair', 'Non-Collared Shirt', 'No Outerwear', 'images/Tina.png');
 
 var characterArray = [adam, beth, cyril, grandpa, krieger, linda, lois, meg, mort, morty, pam, peter, ray, ron, summer, 
 	archer, bob, fischoeder, goldenfold, hugo, jessica, malory, rick, steve, tina];
@@ -79,6 +79,8 @@ $(function() {
 
 			player1Array.push(rand);
 		}
+
+		console.log(player1Array);
 	}
 
 
@@ -91,6 +93,8 @@ $(function() {
 
 			player2Array.push(rand);
 		}
+
+		console.log(player2Array);
 	}
 
 	function populate() {
